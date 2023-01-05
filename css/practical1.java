ffbd3931a3f3d49de318313726bf6c61fa444aec
Sahil pawar <63465844+sahilpawar58@users.noreply.github.com>
2023-01-04 20:52:50 -0800

    Update and rename gcd.java to practical1.java

--- css/practical1.java
+++ css/practical1.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
-class gcd {
+class practical1 {
     public static void main(String[] args) {
         int a=464;
         int b=238;
